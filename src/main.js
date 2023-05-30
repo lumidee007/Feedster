e9916c2d2236db61939d34765d835ce62b320a11
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2023-05-30 12:03:42 +0100

    Add hover functionality to +1 btn

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
-  // Add Hover Functionality to the Navigation Menu
+  // Adding Hover Functionality to the Navigation Menu
   $(".menu").on("mouseenter", () => {
     $(".nav-menu").show(1000);
   });
   $(".nav-menu").on("mouseleave", () => {
     $(".nav-menu").hide(1000);
   });
+
+  // Adding Hover Functionality to the +1 btn
+  $(".btn")
+    .on("mouseenter", (event) => {
+      $(event.currentTarget).addClass("btn-hover");
+    })
+    .on("mouseleave", (event) => {
+      $(event.currentTarget).removeClass("btn-hover");
+    });
 });
